34be3c0dd3d476aff5d4c555a3ed75e32225a875
lklosse <136918592+lklosse@users.noreply.github.com>
2023-06-29 23:52:42 +0200

    .

--- test.js
+++ test.js
@@ -49,11 +49,29 @@ function startDisconnect(){
     document.getElementById("messages").innerHTML += "<span> Disconnected </span><br>";
     }
 
-function publishMessage(elem){
-    msg = document.getElementById("message").value;
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
+function publishMessage(elem){
     Message = new Paho.Message(elem.id);
     Message.destinationName = "klossel/test";
-
     client.send(Message);
-    }
+
+    if(elem.id = "walk"){
+        sleep(1000);
+        stepsForward = document.getElementById("stepsF").value;
+        Paho.Message(stepsForward).destinationName = "klossel/test";
+        client.send(Paho.Message(stepsForward));
+
+        sleep(1000);
+        stepsSideways = document.getElementById("stepsS").value;
+        Paho.Message(stepsSideways).destinationName = "klossel/test";
+        client.send(Paho.Message(turnDegrees));
+        
+        sleep(1000);
+        turnDegrees = document.getElementById("stepsF").value;
+        Paho.Message(turnDegrees).destinationName = "klossel/test";
+        client.send(Paho.Message(turnDegrees));
+    } 
+}
